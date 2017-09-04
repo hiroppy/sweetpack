a583fa606f7e123d88bc4f2ee3199c994469ab9f
Yuta Hiroto <git@about-hiroppy.com>
2017-09-04 23:11:06 +0900

    test: stub for process.cwd

--- test/lib/setting-files/webpack.js
+++ test/lib/setting-files/webpack.js
@@ -1,5 +1,7 @@
 import test from 'ava';
+import '../../_setup';
 import webpack from '../../../lib/setting-files/webpack.config';
+
 
 test('dev when arguments is none', (t) => {
   t.snapshot(webpack.createDev());
