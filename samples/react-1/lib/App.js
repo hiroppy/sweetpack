8ffb4c6c6ddab568fe7b098b36aacdee55e967e9
Yuta Hiroto <git@about-hiroppy.com>
2017-09-07 08:39:19 +0900

    chore(sample): add new sample

---
+++ samples/react-1/lib/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './style.css';
+
+const App = () => (
+  <div>
+    <h1 className={styles.title}>sample</h1>
+    <input />
+    <div className={styles.js} />
+  </div>
+);
+
+export default App;
